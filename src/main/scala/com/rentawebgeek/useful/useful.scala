1ba50a37a806910bd87d2ac5c6523337ffab0a1f
Jeremy Fisher <jeremy@rentawebgeek.com>
2013-11-07 07:38:13 -0600

    Comments, refactoring

--- src/main/scala/com/rentawebgeek/useful/useful.scala
+++ src/main/scala/com/rentawebgeek/useful/useful.scala
@@ -5,23 +5,68 @@ package com.rentawebgeek.useful
 import com.rentawebgeek.dot
 
 
-object useful {
-  implicit def string2Actor(str: String) = new Actor(str)
-  implicit def string2UseCase(str: String) = new UseCase(str) 
-
-  private var graph = new dot.Graph
-  
-  def defaultGraph = graph
-  def defaultGraph_=(newGraph: dot.Graph) = graph = newGraph
+package object useful {
 
   def main(args: Array[String]) = {
+    import Implicits._
     "Web User" uses "Purchase Item"
     "Purchase Item" includes "Login"
     "Log Debugging Info" expands "Login"
     "Order Processing" realizes "Purchase Item"
     //"Synchronize Data" generalizes "Synchronize Serially"
     "Synchronize Wirelessly" specializes "Synchronize Data"
-    print(graph.render())
+    print(DefaultGraph().render())
+  }
+
+  def run(filepath: String) {
+    val lines = scala.io.Source.fromFile(filepath).mkString
+    val sb = new StringBuilder
+    sb.append("import com.rentawebgeek.useful.Implicits._\n")
+    sb.append(lines)
+
+  }
+}
+
+object Implicits {
+  /** Implicit conversion from a string to an actor
+   *
+   * === Usage ===
+   * {{{
+   * scala> import com.rentawebgeek.useful.Implicits._
+   * scala> import com.rentawebgeek.useful._
+   * scala> "Web User" uses UseCase("Login")
+   * }}}
+   */
+  implicit def string2Actor(str: String): Actor = Actor(str)
+  
+  /** Implicitly converts a string to a use case
+   *
+   * === Usage ===
+   * {{{
+   * scala> import com.rentawebgeek.useful.Implicits._
+   * scala> "Order Processing" realizes "Purchase Item"
+   * }}}
+   */
+  implicit def string2UseCase(str: String): UseCase = UseCase(str) 
+
+}
+
+
+/** An implicit "default" graph to make the DSL more approachable */
+object DefaultGraph {
+  private var _graph = new dot.Graph
+  
+  /** Returns the default graph */
+  def apply() = _graph
+  
+  /**
+   *  Assigns the default graph
+   *
+   *  @return the default graph
+   */
+  def apply(graph: dot.Graph) = {
+    _graph = graph
+    _graph
   }
 
 }
@@ -30,22 +75,30 @@ abstract class UseCaseNode extends dot.Node {
 }
 
 class Actor(val name: String) extends UseCaseNode {
-    useful.defaultGraph.add(this)
     attributes += "image" -> "\"actor.svg\""
 
-    def uses(uc: UseCase) = useful.defaultGraph.add(new Relationship(this, uc) with Uses)
-    def initiates(uc: UseCase) = useful.defaultGraph.add(new Relationship(this, uc) with Initiates)
+    def uses(uc: UseCase) = DefaultGraph().add(new Relationship(this, uc) with Uses)
+    def initiates(uc: UseCase) = DefaultGraph().add(new Relationship(this, uc) with Initiates)
 }
+
+object Actor {
+  def apply(name: String): Actor = {
+    val actor = new Actor(name)
+    DefaultGraph().add(actor)
+    return actor
+  }
+}
+
 
 class UseCase(val name: String) extends UseCaseNode {
   private var _isAbstract = false
 
-  def expands(uc: UseCase) = useful.defaultGraph.add(new Relationship(this, uc) with Expands)
-  def includes(uc: UseCase) = useful.defaultGraph.add(new Relationship(this, uc) with Includes)
-  def specializes(uc: UseCase) = useful.defaultGraph.add(new Relationship(this, uc) with Generalizes)
-  def generalizes(uc: UseCase) = useful.defaultGraph.add(new Relationship(uc, this) with Generalizes)
+  def expands(uc: UseCase) = DefaultGraph().add(new Relationship(this, uc) with Expands)
+  def includes(uc: UseCase) = DefaultGraph().add(new Relationship(this, uc) with Includes)
+  def specializes(uc: UseCase) = DefaultGraph().add(new Relationship(this, uc) with Generalizes)
+  def generalizes(uc: UseCase) = DefaultGraph().add(new Relationship(uc, this) with Generalizes)
   def realizes(uc: UseCase) = {
-    useful.defaultGraph.add(new Relationship(this, uc) with Realizes)
+    DefaultGraph().add(new Relationship(this, uc) with Realizes)
     uc.isAbstract = true
   }
 
@@ -54,10 +107,15 @@ class UseCase(val name: String) extends UseCaseNode {
 
 }
 
+object UseCase {
+  def apply(name: String) = new UseCase(name)
+}
 
 class Relationship(override val left: UseCaseNode, override val right: UseCaseNode) extends dot.Edge(left, right) {
 
 }
+
+
 
 trait Uses extends dot.HasAttributes {
   attributes += ("arrowhead" -> "none")
